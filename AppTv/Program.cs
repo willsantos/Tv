d06956c767bbff044f732a1927b94cea7fcd4da0
Wilson Santos <contato@wilsonsantos.net>
2022-06-08 16:14:03 -0300

    ✨ Add metodo atualizar serie

--- AppTv/Program.cs
+++ AppTv/Program.cs
@@ -24,7 +24,7 @@
             InserirSerie();
             break;
           case "3":
-            // AtualizarSerie();
+            AtualizarSerie();
             break;
           case "4":
             ExcluirSerie();
@@ -45,6 +45,36 @@
 
       Console.WriteLine("Obrigado por utilizar nossos serviços. Aperte [Enter] para encerrar");
       Console.ReadLine();
+    }
+
+    private static void AtualizarSerie()
+    {
+      Console.Write("Digite o id da série: ");
+      int indiceSerie = int.Parse(Console.ReadLine());
+
+      foreach (int i in Enum.GetValues(typeof(Genero)))
+      {
+        Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+      }
+      Console.Write("Digite o gênero entre as opções acima: ");
+      int entradaGenero = int.Parse(Console.ReadLine());
+
+      Console.Write("Digite o Título da Série: ");
+      string entradaTitulo = Console.ReadLine();
+
+      Console.Write("Digite o Ano de Início da Série: ");
+      int entradaAno = int.Parse(Console.ReadLine());
+
+      Console.Write("Digite a Descrição da Série: ");
+      string entradaDescricao = Console.ReadLine();
+
+      Serie atualizaSerie = new Serie(id: indiceSerie,
+                    genero: (Genero)entradaGenero,
+                    titulo: entradaTitulo,
+                    ano: entradaAno,
+                    descricao: entradaDescricao);
+
+      repositorio.Atualiza(indiceSerie, atualizaSerie);
     }
 
     private static void ExcluirSerie()
