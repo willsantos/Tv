f78ac38bf4d401d777a8f033656d32e2fe6caec8
Wilson Santos <contato@wilsonsantos.net>
2022-06-08 15:27:33 -0300

    ✨ feat: Add menu opcao do usuario

--- AppTv/Program.cs
+++ AppTv/Program.cs
@@ -7,6 +7,62 @@ namespace AppTv.src
     static void Main(string[] args)
     {
 
+      string opcaoUsuario = ObterOpcaoUsuario();
+
+
+
+      while (opcaoUsuario.ToUpper() != "X")
+      {
+        switch (opcaoUsuario)
+        {
+          case "1":
+            ListarSeries();
+            break;
+          case "2":
+            InserirSerie();
+            break;
+          case "3":
+            // AtualizarSerie();
+            break;
+          case "4":
+            // ExcluirSerie();
+            break;
+          case "5":
+            // VisualizarSerie();
+            break;
+          case "C":
+            Console.Clear();
+            break;
+
+          default:
+            throw new ArgumentOutOfRangeException();
+        }
+
+        opcaoUsuario = ObterOpcaoUsuario();
+      }
+
+      Console.WriteLine("Obrigado por utilizar nossos serviços. Aperte [Enter] para encerrar");
+      Console.ReadLine();
+    }
+    private static string ObterOpcaoUsuario()
+    {
+      Console.WriteLine();
+
+      Console.WriteLine("Informe a opção desejada:");
+
+      Console.WriteLine("1- Listar séries");
+      Console.WriteLine("2- Inserir nova série");
+      Console.WriteLine("3- Atualizar série");
+      Console.WriteLine("4- Excluir série");
+      Console.WriteLine("5- Visualizar série");
+      Console.WriteLine("C- Limpar Tela");
+      Console.WriteLine("X- Sair");
+      Console.WriteLine();
+
+      string opcaoUsuario = Console.ReadLine().ToUpper();
+      Console.WriteLine();
+      return opcaoUsuario;
+    }
     }
   }
 
